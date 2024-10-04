480470ecc2b07c36ad39a8559f25340ff3fec8ca
Austin Cefalu <165092191+austin-cefalu@users.noreply.github.com>
2024-10-03 22:33:00 -0700

    Finalize Roblox OAuth2 flow (to be refactored)

--- django/main/registration/views.py
+++ django/main/registration/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
-from django.http import JsonResponse
-from .utils import generate_code_verifier
+from django.http import JsonResponse, HttpResponse
+from .utils import generate_code_verifier, generate_code_challenge
 from rest_framework.decorators import api_view
+import requests
 from django.conf import settings
+from django.middleware.csrf import get_token
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+#from django.middleware.csrf import get_token
 # Create your views here.
 
 
 @api_view(['GET'])
+@ensure_csrf_cookie
 def VerifyRoblox(request):
     redirect_uri = "http://localhost:8000/accounts/roblox/redirect"
-    state = "STATE_HERE"
-    code_challenge = generate_code_verifier()
+    state = get_token(request)
+    request.session['state'] = state
+    code_verifier = generate_code_verifier()
+    code_challenge = generate_code_challenge(code_verifier)
+
+    request.session['code_verifier'] = code_verifier
     
     final_url = (
         f'https://apis.roblox.com/oauth/v1/authorize'
@@ -35,11 +45,28 @@ def VerifyRoblox(request):
 def VerifyRobloxCallback(request):
     code = request.GET.get('code')
     state = request.GET.get('state')
-    
+
+    if state != request.session['state']:
+        return JsonResponse({"error": "State does not match. Possible CSRF attack!"}, status=403)
     if not code:
         return JsonResponse({"error": "No code provided"}, status=400)
     
-    print(code)
-    print(state)
+    token_url = 'https://apis.roblox.com/oauth/v1/token'
 
-    return JsonResponse({"msg": "hellloooo"}, status=200)
+    payload = {
+        'client_id': settings.ROBLOX_CLIENT_ID,
+        'client_secret': settings.ROBLOX_CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'code_verifier': request.session['code_verifier']
+    }
+
+    response = requests.post(token_url, data=payload)
+
+    if response.status_code == 200:
+        token_data = response.json()
+        print("Access Token:", token_data.get('access_token'))
+        return HttpResponse(status=200)
+    else:
+        print("Error:", response.status_code, response.json())
+        return HttpResponse(status=400)
