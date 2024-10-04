62491cb6a1069d55265aef85d04b7e0f440e56ba
Austin Cefalu <165092191+austin-cefalu@users.noreply.github.com>
2024-10-03 18:21:22 -0700

    Setup PostgreSQL DB config with .env variables

--- django/main/main/settings.py
+++ django/main/main/settings.py
@@ -31,6 +31,8 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'registration.CustomUser'
+
 
 # Application definition
 
@@ -78,10 +80,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
