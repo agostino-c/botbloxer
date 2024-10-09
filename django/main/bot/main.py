2d3a9e6d9d73ca30b7bf99dcec02e3bd5911b953
Austin Cefalu <165092191+austin-cefalu@users.noreply.github.com>
2024-10-09 16:38:27 -0700

    Extended Discord Bot client to be more versatile and added slash command support

--- django/main/bot/main.py
+++ django/main/bot/main.py
@@ -1,28 +1,52 @@
 import discord
+from discord.ext import commands # Commands Module for discord.py
 import os
 from dotenv import load_dotenv
+from discord import app_commands
 
 load_dotenv()
 
-class DiscordClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+# class DiscordClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+#     async def on_message(self, message):
+#         print(f'Message from {message.author}: {message.content}')
 
-    async def on_member_join(self, member):
-        guild = member.guild
-        if guild.system_channel is not None: # Change this to search for a "welcome" channel set by user if not create one
-            message = f'Welcome {member.mention} to {guild.Name}!'
-            await guild.system_channel.send(message)
+#     async def on_member_join(self, member):
+#         guild = member.guild
+#         if guild.system_channel is not None: # Change this to search for a "welcome" channel set by user if not create one
+#             message = f'Welcome {member.mention} to {guild.Name}!'
+#             await guild.system_channel.send(message)
         
 
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = DiscordClient(intents=intents)
+# client = DiscordClient(intents=intents)
 
 
-client.run(os.getenv('DISCORD_BOT_SECRET'))
+
+
+# client.run(os.getenv('DISCORD_BOT_SECRET'))
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.hybrid_command()
+async def ping(ctx: commands.Context):
+    """Says pong!"""
+    await ctx.send('pong!')
+
+# @bot.hybrid_command()
+# async def verify()
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.tree.sync()
+
+
+bot.run(os.getenv('DISCORD_BOT_SECRET'))
