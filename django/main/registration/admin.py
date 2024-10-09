59a9db66e7f50b355b7b3cbbacd60253303cda30
Austin Cefalu <165092191+austin-cefalu@users.noreply.github.com>
2024-10-09 13:17:16 -0700

    Fixed incorrect parameters being used in methods such as create_user(), create_super_user() that were caused by a sleepy dev. Also set-up the RobloxUser model in the Django ORM

--- django/main/registration/admin.py
+++ django/main/registration/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from .models import RobloxUser
 UserBase = get_user_model()
 
 # Admin Panel Site Config
@@ -9,3 +10,4 @@ admin.site.site_header = "Django Administration"
 
 # Register your models here.
 admin.site.register(UserBase)
+admin.site.register(RobloxUser)
--- django/main/registration/models.py
+++ django/main/registration/models.py
@@ -3,24 +3,22 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
-    def create_user(self, robloxID, username, password):
-        if not username:
-            raise ValueError("User must have a valid username")
-        if not robloxID:
-            raise ValueError('User must have a valid Roblox User ID')
-        
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("User must have a valid email")
         if not password:
             raise ValueError("No password was provided")
-        
-        user = self.model(robloxID=robloxID, username=username)
+
+        user = self.model(email=email)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, robloxID, username, password=None, **kwargs):
-        kwargs['password'] = password
-        user = self.create_user(robloxID, username, **kwargs)
+    def create_superuser(self, email, password=None):
+        # user = self.create_user(robloxID, username, **kwargs)
+        user = self.create_user(email, password)
+
         user.is_superuser = True
         user.is_staff = True
 
@@ -30,19 +28,30 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    robloxID = models.CharField(max_length=255, unique=True)
-    username = models.CharField(max_length=125)
-
+    email = models.EmailField(unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'robloxID'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return self.username
+        return self.email
     
     class Meta:
+        # Verbose Name for Django Admin
         verbose_name = 'Django User'
+
+
+class RobloxUser(models.Model):
+    robloxID = models.CharField(max_length=255, unique=True)
+    isBanned = models.BooleanField(default=False)
+    banHistory = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.robloxID
+    
+    class Meta:
+        # Verbose Name for Django Admin
+        verbose_name = 'Members'
