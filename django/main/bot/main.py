38eee23b9b71d819eb314f444bceffc2de395506
Austin Cefalu <165092191+austin-cefalu@users.noreply.github.com>
2024-10-09 14:59:05 -0700

    Added on_member_join() async for when a new user joins a Discord Guild

--- django/main/bot/main.py
+++ django/main/bot/main.py
@@ -11,6 +11,12 @@ class DiscordClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None: # Change this to search for a "welcome" channel set by user if not create one
+            message = f'Welcome {member.mention} to {guild.Name}!'
+            await guild.system_channel.send(message)
+        
 
 
 
